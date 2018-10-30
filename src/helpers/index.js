95b6febb24c48e483f8094f520ab5c3da459466b
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-30 19:20:16 -0400

    added a logger

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -9,3 +9,12 @@ export function getUniq(array, getItem) {
   }
   return item;
 }
+
+const label = ([raw]) => {
+  const [color, label, ...message] = raw.split(' ')
+  return [
+      `%c${label}%c ${message.join(' ')}`,
+      `color: white; background-color: ${color}; padding: 0 .5em 0 .5em`,
+      ''
+  ]
+}
