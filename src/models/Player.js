174b7263621cf440f77357c4c86719eea6180f02
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-11-17 16:13:23 -0500

    added Player model

---
+++ src/models/Player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor({ name, position }) {
+    this.name = name;
+    this.position = position;
+  }
+
+  skills = {
+    offense: 0,
+    defence: 0,
+    teamwork: 0,
+    shooting: 0,
+    passing: 0,
+    aggression: 0,
+    speed: 0,
+    strength: 0
+  };
+}
+
+export default Team;
--- src/models/Team.js
+++ src/models/Team.js
@@ -8,6 +8,18 @@ class Team {
   get teamName() {
     return `${this.city} ${this.name}`;
   }
+
+  points = {
+    wins: 0,
+    losses: 0,
+    ot: 0, //overtime losses
+    sf: 0, //shots for
+    sa: 0, //shots against
+    gf: 0, //goals for
+    ga: 0, //goals against
+    pim: 0, //penalty minutes
+    so: 0 //shut-outs
+  };
 }
 
 export default Team;
